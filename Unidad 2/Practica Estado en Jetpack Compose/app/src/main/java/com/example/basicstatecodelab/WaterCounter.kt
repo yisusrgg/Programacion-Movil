572b7e279bc3a064070f728e3edea9ebcd4145c9
Jesús Rosiles González <jesusrosilesg18@gmail.com>
2025-09-18 11:30:51 -0600

    Cómo recordar en la composición

--- Unidad 2/Practica Estado en Jetpack Compose/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ Unidad 2/Practica Estado en Jetpack Compose/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -10,15 +10,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Row
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
-
         if (count > 0) {
-            // This text is present if the button has been clicked
-            // at least once; absent otherwise
             Text("You've had $count glasses.")
         }
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
