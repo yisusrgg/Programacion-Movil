fc9615a63b12e8e8ce4e45fd4301d89d57382b26
Jesús Rosiles González <jesusrosilesg18@gmail.com>
2025-09-18 00:52:20 -0600

    Memoria en una función de componibilidad

--- Unidad 2/Practica Estado en Jetpack Compose/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ Unidad 2/Practica Estado en Jetpack Compose/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -3,13 +3,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        var count by remember { mutableStateOf(0) }
+
         Text("You've had $count glasses.")
         Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
