88cf53822f06bbbbe385c544c458432d7b6d25d6
Jesús Rosiles González <jesusrosilesg18@gmail.com>
2025-09-19 11:40:34 -0600

    MutableList observable

--- Unidad 2/Practica Estado en Jetpack Compose/app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
+++ Unidad 2/Practica Estado en Jetpack Compose/app/src/main/java/com/example/basicstatecodelab/WellnessTaskItem.kt
@@ -45,14 +45,18 @@ fun WellnessTaskItem(
 }
 
 @Composable
-fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+fun WellnessTaskItem(
+    taskName: String,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     var checkedState by rememberSaveable { mutableStateOf(false) }
 
     WellnessTaskItem(
         taskName = taskName,
         checked = checkedState,
         onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = {}, // todavía no lo implementamos
+        onClose = onClose,
         modifier = modifier,
     )
 }
